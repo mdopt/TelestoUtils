ebaa0dd1035da37904c3210f18359fee8c5ca349
Pawel Ulewicz <pawel.ulewicz@gmail.com>
2014-05-11 14:04:44 +0200

    Added type checking methods to TypeUtil

--- src/TypeUtil.php
+++ src/TypeUtil.php
@@ -8,6 +8,14 @@ use RuntimeException;
 abstract class TypeUtil
 {
     /**
+     * Types that can be checked easily with is_* function
+     */
+    protected static $basicTypes = array(
+        'bool', 'int', 'float', 'string', 'array', 'scalar',
+        'null', 'object', 'callable', 'resource'
+    );
+    
+    /**
      * Tells if a class or an interface with given name exists.
      *
      * @param   string      $typeName       Name of a class or an interface
@@ -36,5 +44,60 @@ abstract class TypeUtil
     public static function getType($input)
     {
         return is_object($input)? get_class($input) : gettype($input);
+    }
+    
+    /**
+     * @param   mixed       $input
+     * @param   string      $type       Type name or type names separated by '|'
+     *                                  Example 'string|null'
+     * @return  bool
+     */
+    public static function isType($input, $type)
+    {
+        $allowedTypes = explode('|', $type);
+        $isObject = is_object($input);
+        
+        foreach ($allowedTypes as $allowedType) {
+            $typeTest = in_array($allowedType, static::$basicTypes, true)? 'is_' . $allowedType : null;
+            
+            if ($typeTest && call_user_func($typeTest, $input)) {
+                return true;
+            }
+            else if ($isObject && is_a($input, $allowedType)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * @param   mixed       $input
+     * @param   string      $type           Type name or type names separated by '|'
+     *                                      Example 'string|null'
+     *
+     * @param   string      $argumentName
+     * @param   array       $options
+     *
+     * @return  void
+     *
+     * @throws  InvalidArgumentException    be default
+     * @throws  Exception                   any other type of exception if specified in $options
+     */
+    public static function requireType($input, $type, $argumentName, array $options = array())
+    {
+        if (static::isType($input, $type)) {
+            return;
+        }
+        
+        $actualType     = static::getType($input);
+        $exceptionClass = isset($options['exceptionClass'])? $options['exceptionClass'] : 'InvalidArgumentException';
+        $messageFormat  = isset($options['messageFormat'])? $options['messageFormat'] : 'Invalid argument type for %1$s: %2$s expected, got %3$s.';
+        $code           = isset($options['exceptionCode'])? $options['exceptionCode'] : 0;
+        
+        throw new $exceptionClass(
+            sprintf($messageFormat, $argumentName, $type, $actualType),
+            $code
+        );
     }
 }
--- tests/src/TypeUtilTest.php
+++ tests/src/TypeUtilTest.php
@@ -91,4 +91,112 @@ class TypeUtilTest extends \PHPUnit_Framework_TestCase
             )
         );
     }
+    
+    /**
+     * @dataProvider provideIsTypeData
+     */
+    public function testIsType($expectedValue, $input, $type)
+    {
+        $this->assertSame($expectedValue, TypeUtil::isType($input, $type));
+    }
+    
+    public function provideIsTypeData()
+    {
+        return array(
+            array(
+                true,
+                10,
+                'int'
+            ),
+            array(
+                true,
+                null,
+                'int|null'
+            ),
+            array(
+                false,
+                null,
+                'string'
+            ),
+            array(
+                false,
+                null,
+                'string|int|array'
+            ),
+            array(
+                true,
+                'John Doe',
+                'string|int|array'
+            ),
+            array(
+                true,
+                array(10, 20),
+                'array'
+            ),
+            array(
+                false,
+                new \stdClass,
+                'array'
+            ),
+            array(
+                true,
+                new \stdClass,
+                'stdClass'
+            )
+        );
+    }
+    
+    /**
+     * @dataProvider provideRequireTypeData
+     */
+    public function testRequireType(
+        $expectedException,
+        $input,
+        $type,
+        $argumentName,
+        $options = array()
+    ) {
+        if ($expectedException) {
+            $this->setExpectedException($expectedException[0], $expectedException[1], $expectedException[2]);
+        }
+        
+        TypeUtil::requireType($input, $type, $argumentName, $options);
+    }
+    
+    public function provideRequireTypeData()
+    {
+        return array(
+            array(
+                array(
+                    'InvalidArgumentException',
+                    'Invalid argument type for $radius: int|null expected, got stdClass.',
+                    0
+                ),
+                new \stdClass,
+                'int|null',
+                '$radius'
+            ),
+            array(
+                array(
+                    'RuntimeException',
+                    '$radius has invalid type stdClass (expected int|null).',
+                    10
+                ),
+                new \stdClass,
+                'int|null',
+                '$radius',
+                array(
+                    'exceptionClass'    => 'RuntimeException', // RuntimeException doesn't make sense in real situations, but it's just for tests
+                    'exceptionCode'     => 10,
+                    'messageFormat'     => '%1$s has invalid type %3$s (expected %2$s).'
+                )
+            ),
+            array(
+                null,
+                104523,
+                'int|null',
+                '$radius'
+            )
+        );
+    }
 }

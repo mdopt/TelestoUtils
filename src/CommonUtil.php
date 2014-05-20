c7eb26d486dab294cb189ebf1ea67022d25db850
Pawel Ulewicz <pawel.ulewicz@gmail.com>
2014-05-20 12:43:26 +0200

    Moved unserialize method to CommonUtil.

--- src/CommonUtil.php
+++ src/CommonUtil.php
@@ -62,4 +62,44 @@ abstract class CommonUtil
         $reflectionClass = new \ReflectionClass($className);
         return $reflectionClass->newInstanceArgs($arguments);
     }
+    
+    /**
+     * Unserialize method that does type-checking and throws exceptions
+     * instead of reporting E_NOTICE.
+     *
+     * @param   string      $serialized
+     * @param   bool        $suppressNotice
+     *
+     * @return  mixed
+     *
+     * @throws  InvalidArgumentException
+     * @throws  UnexpectedValueException
+     */
+    public static function unserialize($serialized, $suppressNotice = false)
+    {
+        if (!is_string($serialized)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Serialized value must be a string, %s given.',
+                    is_object($serialized)? get_class($serialized) : gettype($serialized)
+                )
+            );
+        }
+        
+        if ($suppressNotice) {
+            set_error_handler(function () {}, E_NOTICE);
+        }
+        
+        $unserialized = unserialize($serialized);
+        
+        if ($suppressNotice) {
+            restore_error_handler();
+        }
+        
+        if ($unserialized === false && $serialized !== serialize(false)) {
+            throw new UnexpectedValueException('Invalid value after unserialization.');
+        }
+        
+        return $unserialized;
+    }
 }
--- tests/src/CommonUtilTest.php
+++ tests/src/CommonUtilTest.php
@@ -84,4 +84,81 @@ class CommonUtilTest extends \PHPUnit_Framework_TestCase
             )
         );
     }
+    
+    /**
+     * @dataProvider provideUnserializeData
+     */
+    public function testUnserialize($expectedResult, $serialized)
+    {
+        if (is_object($expectedResult)) {
+            $this->assertEquals($expectedResult, CommonUtil::unserialize($serialized));
+        }
+        else {
+            $this->assertSame($expectedResult, CommonUtil::unserialize($serialized));
+        }
+    }
+    
+    public function provideUnserializeData()
+    {
+        return array(
+            array(
+                array(1, 2),
+                serialize(array(1, 2))
+            ),
+            array(
+                (object) array('x'=> 10, 'y'=> 20),
+                serialize((object) array('x'=> 10, 'y'=> 20))
+            ),
+            array(
+                false,
+                serialize(false)
+            )
+        );
+    }
+    
+    /**
+     * @dataProvider provideUnserializeExceptionsData
+     */
+    public function testUnserializeExceptions(array $expectedException, $serialized, $hideNotice = false)
+    {
+        $this->setExpectedException($expectedException[0], $expectedException[1]);
+        
+        CommonUtil::unserialize($serialized, $hideNotice);
+    }
+    
+    public function provideUnserializeExceptionsData()
+    {
+        return array(
+            array(
+                array(
+                    'InvalidArgumentException',
+                    'Serialized value must be a string, NULL given.'
+                ),
+                null
+            ),
+            array(
+                array(
+                    'InvalidArgumentException',
+                    'Serialized value must be a string, stdClass given.'
+                ),
+                new \stdClass
+            ),
+            array(
+                array(
+                    'UnexpectedValueException',
+                    'Invalid value after unserialization.'
+                ),
+                'invalid-string',
+                true
+            ),
+            array(
+                array(
+                    'PHPUnit_Framework_Error_Notice',
+                    ''
+                ),
+                'invalid-string',
+                false
+            )
+        );
+    }
 }
